04770f3ec0dedb81c614bf24ada154aa8d1302dd
sami-hamid <samifadlallah21@gmail.com>
2024-03-28 13:15:14 -0700

    0x10-python-network_0

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-SamiFadllah-User-Id: 80" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=samifadlallah21@gmail.com" "$1"
