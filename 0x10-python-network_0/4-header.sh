6f62e92aa59dfdd3857019d8ea7373a0a1f892f1
sami-hamid <83097392+sami-hamid@users.noreply.github.com>
2024-03-29 01:02:01 +0400

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/4-header.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-SamiFadllah-User-Id: 80" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
-curl -s -X POST -d "email=samifadlallah21@gmail.com" "$1"
